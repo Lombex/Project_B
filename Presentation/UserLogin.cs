634fe3c22c4feb4a0af4062f0ed9d711bf76ccb8
Lucasdehaas <de.haas.0605@gmail.com>
2023-04-02 00:47:22 +0200

    Made MakeAccount Method

--- Presentation/UserLogin.cs
+++ Presentation/UserLogin.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 static class UserLogin
 {
     static private AccountsLogic accountsLogic = new AccountsLogic();
@@ -33,10 +34,9 @@ static class UserLogin
         string full_name = Console.ReadLine();
         Console.WriteLine("Please enter your email address");
         string email = Console.ReadLine();
-        // Check if email is already in json! 
         Console.WriteLine("Please enter your password");
         string password_1 = Console.ReadLine();
-        Console.WriteLine("Please enter your again password");
+        Console.WriteLine("Please enter your password again");
         string password_2 = Console.ReadLine();
 
         if (password_1 != password_2)
@@ -44,8 +44,24 @@ static class UserLogin
             Console.WriteLine("Password is not the same, please try again..");
             UserLogin.MakeAccount();
         }
+        else
+        {
+            string reader = File.ReadAllText("DataSources/accounts.json");
+            List<AccountModel> dataList = JsonConvert.DeserializeObject<List<AccountModel>>(reader);
+            if (dataList.Any(data => data.EmailAddress == email))
+                Console.WriteLine("An account with the same email address already exists. Please choose a different email address.");
 
-        // Make function in logic layer that adds this account to json 
-
+            else
+            {
+                int highestId = dataList.Max(data => data.Id);
+                AccountModel newData = new AccountModel(highestId + 1, email, password_1, full_name);
+                dataList.Add(newData);
+                string updatedJson = JsonConvert.SerializeObject(dataList, Formatting.Indented);
+                StreamWriter writer = new("DataSources/accounts.json");
+                writer.Write(updatedJson);
+                writer.Close();
+            }
+            Menu.Start();
+        }
     }
 }
