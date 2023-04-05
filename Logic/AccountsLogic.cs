446b33246f8897328ce0da937a9a77088396990a
1052307-hr <1052307@hr.nl>
2023-04-05 14:35:32 +0200

    Moving login logic from presentation to logic layer.

--- Logic/AccountsLogic.cs
+++ Logic/AccountsLogic.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Text.Json;
-
+using System.Security.Cryptography;
 
 //This class is not static so later on we can use inheritance and interfaces
 class AccountsLogic
@@ -44,12 +45,23 @@ class AccountsLogic
         return _accounts.Find(i => i.Id == id);
     }
 
+    public static string GetHashedSHA256(string password)
+    {
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++) builder.Append(bytes[i].ToString("x2"));
+            return builder.ToString();
+        }
+    }
     public AccountModel? CheckLogin(string email, string? password)
     {
         if (email == null || password == null) throw new Exception("Either email or password is null");
         CurrentAccount = _accounts.Find(i => i.EmailAddress == email && i.Password == password);
         return CurrentAccount;
     }
+
 }
 
 
