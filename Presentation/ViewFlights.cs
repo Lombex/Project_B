916c8a634e3647c5b475f7af835bc1367292fa74
Lucasdehaas <de.haas.0605@gmail.com>
2023-04-20 13:37:45 +0200

    made ViewFlights

--- Presentation/ViewFlights.cs
+++ Presentation/ViewFlights.cs
@@ -4,13 +4,18 @@ public class ViewFlights
     public static void Menu()
     {
         Console.WriteLine("Welcome to Book a flight!");
-        ConsoleTable Table = new ConsoleTable("Row", "A", "B", "", "C", "D", "", "E", "F");
-        for (int row = 1; row < 30; row++)
+        ConsoleTableOptions options = new ConsoleTableOptions
         {
-            Table.AddRow("Seat Number", row, row, "", row, row, "", row, 1).AddRow("TESt", "Test1", "Test2", "", "Test3", "Test3", "", "Test3", "Test3");
+            EnableCount = false
+        };
+        ConsoleTable Table = new ConsoleTable("Row", "A", "B", "", "C", "D", "", "E", "F");
+        Table.Options.EnableCount = options.EnableCount;
+        for (int row = 1; row < 31; row++)
+        {
+            Table.AddRow("Seat Number", row, row, "", row, row, "", row, row);
         }
 
-        Console.WriteLine(Table);
+        Console.WriteLine(Table.ToString());
 
     }
     // Create method filter flight by catagory
