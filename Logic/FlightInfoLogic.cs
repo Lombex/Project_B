301bd8e49a8af9f9e020b9307b6fb6dc63a2cc0c
Lucasdehaas <de.haas.0605@gmail.com>
2023-04-20 14:11:57 +0200

    Made methods for Filter

--- Logic/FlightInfoLogic.cs
+++ Logic/FlightInfoLogic.cs
@@ -16,7 +16,17 @@ public class FlightInfoLogic
         _flightinfo = FlightInfoAccess.LoadAll();
     }
 
-    
+    public FlightInfoModel? GetByFlightNumber(string searchTerm) => _flightinfo.Find(i => i.FlightNumber == searchTerm);
+    public FlightInfoModel? GetByAircraft(string searchTerm) => _flightinfo.Find(i => i.Aircraft == searchTerm);
+    public FlightInfoModel? GetByOrigin(string searchTerm) => _flightinfo.Find(i => i.Origin == searchTerm);
+    public FlightInfoModel? GetByDestination(string searchTerm) => _flightinfo.Find(i => i.Destination == searchTerm);
+    public FlightInfoModel? GetByDate(string searchTerm) => _flightinfo.Find(i => i.Date == searchTerm);
+    public FlightInfoModel? GetByFlightTime(double searchTerm) => _flightinfo.Find(i => i.FlightTime == searchTerm);
+    public FlightInfoModel? GetByDepartTime(string searchTerm) => _flightinfo.Find(i => i.DepartTime == searchTerm);
+    public FlightInfoModel? GetByArrivalTime(string searchTerm) => _flightinfo.Find(i => i.ArrivalTime == searchTerm);
+    public FlightInfoModel? GetByGate(string searchTerm) => _flightinfo.Find(i => i.Gate == searchTerm);
+
+
 
 
 
