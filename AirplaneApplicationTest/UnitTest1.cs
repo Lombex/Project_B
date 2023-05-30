13376a507f3c9aa7db8707ce950b1fd45353c4a8
1052307-hr <1052307@hr.nl>
2023-05-30 14:23:33 +0200

    Start of test from user input

--- AirplaneApplicationTest/UnitTest1.cs
+++ AirplaneApplicationTest/UnitTest1.cs
@@ -8,6 +8,8 @@ namespace AirplaneApplicationTest
     {
         // T1 (REQ F4.1) -> Try to create account with a valid email and password, but also with invalid information. 
         [TestMethod]
+
+        // use Console.In for input
         public void Check_if_input_null_gives_right_exception()
         {
             AccountsLogic account_logic = new AccountsLogic();
@@ -20,6 +22,26 @@ namespace AirplaneApplicationTest
             {
                 Assert.IsTrue(ex.Message.Contains("Either email or password is null"));
             }
+        }
+
+        [TestMethod]
+        public void Check_logout()
+        {
+            // start van een idee mr vgm werkt dit niet
+            bool succeeded;
+            process.StartInfo.RedirectStandardInput = true;
+            MainSpace.Main();
+            process.StandardInput.WriteLine("3");
+            // Assert.
+
+            /* example for timeouts:
+
+            var task = Task.Run(() => SomeMethod(input));
+            if (task.Wait(TimeSpan.FromSeconds(10)))
+                return task.Result;
+            else
+                throw new Exception("Timed out");
+            */
         }
 
         public void Check_for_creating_new_user_account()
