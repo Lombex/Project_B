00067e5a2c33a819edd35ba199b9a84971193c24
Naud14 <n.drost14@gmail.com>
2023-06-01 16:50:51 +0200

    Fix FlightID in sorting system

--- Presentation/ViewFlights.cs
+++ Presentation/ViewFlights.cs
@@ -195,7 +195,7 @@ public class ViewFlights
         FlightTable.Options.EnableCount = options.EnableCount;
         for (int count = 0; count < _flights!.Count; count++)
         {
-            FlightTable.AddRow(count + 1, _flights[count].FlightNumber, _flights[count].Aircraft, _flights[count].Origin, _flights[count].Destination, _flights[count].Date, _flights[count].FlightTime,
+            FlightTable.AddRow(_flights[count].FlightID + 1, _flights[count].FlightNumber, _flights[count].Aircraft, _flights[count].Origin, _flights[count].Destination, _flights[count].Date, _flights[count].FlightTime,
             _flights[count].DepartTime, _flights[count].ArrivalTime, _flights[count].Gate);
         }
         Console.Clear();
