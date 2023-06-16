4ed7d2a6d58806dd4c9f7aaf2fe5146beea666e0
Lombex <Lombex@users.noreply.github.com>
2023-06-16 10:06:20 +0200

    Test

--- Logic/User.cs
+++ Logic/User.cs
@@ -97,7 +97,63 @@ public class User
             ChangePassword();
         }
     }
+
     public void ChangeEmail(bool isAdmin = false)
+    {
+        if (!isAdmin)
+        {
+            if (!CheckPassword())
+            {
+                AccountFunctionality.ErrorMessage("This password is incorrect, please try again.");
+                ChangeEmail();
+                return;
+            }
+        }
+        else
+        {
+            string EmailAddress = AccountFunctionality.GetInput("Give the user's email address: ");
+            var account = ViewFlights.accountList.FirstOrDefault(a => a.EmailAddress == EmailAddress);
+            if (account != null)
+            {
+                Console.WriteLine("Give user's new Email: ");
+                string NewName = Console.ReadLine()!;
+                account!.EmailAddress = NewName;
+                ViewFlights.accountList[account.Id - 1] = account;
+                accountsAccess.WriteAll(ViewFlights.accountList);
+                Console.WriteLine("User has been changed!");
+                Console.Write("\nPress Enter to continue...");
+                Console.ReadLine();
+                Menu.AdminAccount();
+            }
+            else
+            {
+                AccountFunctionality.ErrorMessage("Could not find that email, please try again!");
+                ChangeEmail(isAdmin);
+            }
+        }
+        string EmailName = AccountFunctionality.GetInput("What name do you want to use? ");
+        string ConfirmEmail = AccountFunctionality.GetInput("Please enter your name again.");
+        if (EmailName == ConfirmEmail)
+        {
+            // uses TextInfo to turn names from "john doe" to "John Doe"
+            var textinfo = new CultureInfo("en-US", false).TextInfo;
+            EmailName = textinfo.ToTitleCase(EmailName.ToLower());
+
+            Console.WriteLine($"Email has been changed to {EmailName}");
+            UserLogin.AccountInfo!.EmailAddress = EmailName;
+            ViewFlights.accountList[UserLogin.AccountInfo.Id - 1] = UserLogin.AccountInfo;
+            accountsAccess.WriteAll(ViewFlights.accountList);
+            Console.Write("Press Enter to continue...");
+            Console.ReadLine();
+        }
+        else
+        {
+            AccountFunctionality.ErrorMessage("The email given don't match.");
+            ChangeEmail();
+        }
+    }
+
+    /*public void ChangeEmail(bool isAdmin = false)
     {
         string NewEmail = "";
         string ConfirmEmail = "";
@@ -144,7 +200,7 @@ public class User
             }
         }
 
-    }
+    }*/
 
     private bool CheckPassword(bool isAdmin = false)
     {
